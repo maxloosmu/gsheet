f5595b6660c6c90a1a1589790d8bc4ab4073c826
maxloosmu <maxloo@smu.edu.sg>
2021-11-19 20:48:01 +0800

    moved Legal Test 13-16 to /more, amended Legal Test 18 and README, added Legal Test 19

--- Legal Test 19.js
+++ Legal Test 19.js
@@ -1,6 +1,6 @@
 function testEdit() {
   let sheet = SpreadsheetApp.getActiveSheet();
-  let c = sheet.getRange('A2');
+  let c = sheet.getRange('A3');
   let cvalue = c.getValue();
   let h = sheet.getRange('A1');
   h.history = [];
@@ -22,8 +22,133 @@ function onEdit(e) {
   h.history = [];
   h.farCol = 0;
   let summary = ["="];
+  drawWords(c, summary, h);
   parseWords(c, summary, h);
   processHistory(h, sheet);
+}
+function drawWords(c, summary, h) {
+  cvalue = c.getValue();
+  if (cvalue=="IF" || cvalue=="OR") {
+    drawIfOr(c);
+  }
+  else if (cvalue=="AND") {
+    drawAnd(c);
+  }
+}
+function parseWords(c, summary, h) {
+  // identify keywords, draw their layout
+  // and process them into h.history and summary.
+  cvalue = c.getValue();
+  if (cvalue=="IF") {
+    processIf(c, summary, h);
+  }
+  else if (cvalue=="AND" || cvalue=="OR") {
+    processAndOr(c, summary, h);
+  }
+}
+function processIf(c, summary, h) {
+  let rightOfIf = c.offset(0,1);
+  let predValue = rightOfIf.getValue();
+  let topOfIf = c.offset(-1,0);
+  if (topOfIf.isBlank()) {
+    topOfIf.insertCheckboxes();
+  }
+  h.history[c.getRowIndex()] =
+    [c.getColumnIndex(), c.getValue(), predValue];
+  h.farCol = getFurthest(h.farCol, c.getColumnIndex());
+  let boxRightOfIf =
+    rightOfIf.getDataValidation().getCriteriaType();
+  if (boxRightOfIf=="CHECKBOX") {
+    summary.push(" (" + predValue);
+    let [nextCell, endParse, _] = getNextCell(c);
+    if (!endParse) {
+      parseWords(nextCell, summary, h);
+    } else {
+      summary.push(")");
+    }
+  }
+  Logger.log("the end " + summary);
+}
+function processAndOr(c, summary, h){
+  let rightOfIf = c.offset(0,1);
+  let predValue = rightOfIf.getValue();
+  let cvalue = c.getValue();
+  h.history[c.getRowIndex()] =
+    [c.getColumnIndex(), cvalue, predValue];
+  let boxRightOfIf =
+    rightOfIf.getDataValidation().getCriteriaType();
+  if (boxRightOfIf=="CHECKBOX") {
+    summary.push(` ${cvalue} ${predValue}`);
+    let [nextCell, endParse, cellCol] = getNextCell(c);
+    const closeBracket = function() {
+      while (cellCol<0) {
+        summary.push(")");
+        cellCol++;
+      }
+    }
+    if (!endParse && cellCol>=0) {
+      parseWords(nextCell, summary, h);
+    }
+    else if (!endParse && cellCol<0) {
+      closeBracket();
+      parseWords(nextCell, summary, h);
+    }
+    else if (endParse) {
+      closeBracket();
+    }
+  }
+}
+function getNextCell(cell) {
+  // get cell with value in next row.  if no more
+  // cells with value, tag and trigger endParse.
+  let nextCell;
+  let nextCValue = "";
+  let cellCol = 1;
+  let endParse = false;
+  let columnLimit = 1 - cell.getColumnIndex();
+  do {
+    nextCell = cell.offset(1,cellCol);
+    nextCValue = nextCell.getValue();
+    if (nextCValue=="IF" || nextCValue=="AND"
+      || nextCValue=="OR") {
+      break;
+    }
+    cellCol -= 1;
+  } while (cellCol >= columnLimit)
+  if (!nextCell.getValue()) {
+    endParse = true;
+    Logger.log("endParse = " + endParse);
+  }
+  return [nextCell, endParse, cellCol];
+}
+function drawIfOr(c) {
+  c.setHorizontalAlignment("right");
+  if (c.offset(0,1).isBlank()) {
+    c.offset(0,1).insertCheckboxes()
+      .setBorder(false,true,false,false,false,false,
+      "grey",SpreadsheetApp.BorderStyle.SOLID_THICK);
+    c.setBorder(null,null,null,true,false,false,
+      "grey",SpreadsheetApp.BorderStyle.SOLID_THICK);
+  }
+  if (c.offset(0,2).isBlank()) {
+    c.offset(0,2).setValue("some condition");
+  }
+}
+function drawAnd(c) {
+  c.setHorizontalAlignment("right");
+  if (c.offset(0,1).isBlank()) {
+    c.offset(0,1).insertCheckboxes()
+      .setBorder(null,true,false, false, false,false,
+      "grey",SpreadsheetApp.BorderStyle.SOLID_THICK);
+    c.offset(0,1,1,4)
+      .setBorder(true,true,false,false,false,false,
+      "grey",SpreadsheetApp.BorderStyle.SOLID_THICK);
+    c.setBorder(null,null,null,true,false,false,
+      "grey",SpreadsheetApp.BorderStyle.SOLID_THICK);
+  }
+  if (c.offset(0,2).isBlank()) {
+    c.offset(0,2).setValue("some condition");
+  }
 }
 function processHistory(h, sheet) {
   // process the h.history Array.
@@ -66,156 +191,4 @@ function getFurthest(prevIndex, index) {
     prevIndex = index;
   }
   return prevIndex;
-}
-function drawWords(c, summary, h) {
-  cvalue = c.getValue();
-  if (cvalue=="IF" || cvalue=="OR") {
-    drawIfOr(c);
-  }
-  else if (cvalue=="AND") {
-    drawAnd(c);
-  }
-}
-function parseWords(c, summary, h) {
-  // identify keywords, draw their layout
-  // and process them into h.history and summary.
-  cvalue = c.getValue();
-  if (cvalue=="IF") {
-    processIf(c, summary, h);
-  }
-  else if (cvalue=="AND") {
-    processAnd(c, summary, h);
-  }
-  else if (cvalue=="OR") {
-    processOr(c, summary, h);
-  }
-}
-function processIf(c, summary, h) {
-  let rightOfIf = c.offset(0,1);
-  let predValue = rightOfIf.getValue();
-  let topOfIf = c.offset(-1,0);
-  h.history[c.getRowIndex()] =
-    [c.getColumnIndex(), c.getValue(), predValue];
-  h.farCol = getFurthest(h.farCol, c.getColumnIndex());
-  let boxRightOfIf =
-    rightOfIf.getDataValidation().getCriteriaType();
-  if (boxRightOfIf=="CHECKBOX") {
-    summary.push(" (" + predValue);
-    let [nextCell, endParse, _] = getNextCell(c);
-    if (!endParse) {
-      parseWords(nextCell, summary, h);
-    } else {
-      summary.push(")");
-    }
-  }
-  Logger.log("the end " + summary);
-}
-function processAnd(c, summary, h){
-  let rightOfIf = c.offset(0,1);
-  let predValue = rightOfIf.getValue();
-  h.history[c.getRowIndex()] =
-    [c.getColumnIndex(), c.getValue(), predValue];
-  let boxRightOfIf =
-    rightOfIf.getDataValidation().getCriteriaType();
-  if (boxRightOfIf=="CHECKBOX") {
-    summary.push(" AND " + predValue);
-    let [nextCell, endParse, cellCol] = getNextCell(c);
-    const closeBracket = function() {
-      while (cellCol<0) {
-        summary.push(")");
-        cellCol++;
-      }
-    }
-    if (!endParse && cellCol>=0) {
-      parseWords(nextCell, summary, h);
-    }
-    else if (!endParse && cellCol<0) {
-      closeBracket();
-      parseWords(nextCell, summary, h);
-    }
-    else if (endParse) {
-      closeBracket();
-    }
-  }
-}
-function processOr(c, summary, h){
-  let rightOfIf = c.offset(0,1);
-  let predValue = rightOfIf.getValue();
-  h.history[c.getRowIndex()] =
-    [c.getColumnIndex(), c.getValue(), predValue];
-  let boxRightOfIf =
-    rightOfIf.getDataValidation().getCriteriaType();
-  if (boxRightOfIf=="CHECKBOX") {
-    summary.push(" OR " + predValue);
-    let [nextCell, endParse, cellCol] = getNextCell(c);
-    const closeBracket = function() {
-      while (cellCol<0) {
-        summary.push(")");
-        cellCol++;
-      }
-    }
-    if (!endParse && cellCol>=0) {
-      parseWords(nextCell, summary, h);
-    }
-    else if (!endParse && cellCol<0) {
-      closeBracket();
-      parseWords(nextCell, summary, h);
-    }
-    else if (endParse) {
-      closeBracket();
-    }
-  }
-}
-function getNextCell(cell) {
-  // get cell with value in next row.  if no more
-  // cells with value, tag and trigger endParse.
-  let nextCell;
-  let nextCValue = "";
-  let cellCol = 1;
-  let endParse = false;
-  let columnLimit = 1 - cell.getColumnIndex();
-  do {
-    nextCell = cell.offset(1,cellCol);
-    nextCValue = nextCell.getValue();
-    if (nextCValue=="IF" || nextCValue=="AND"
-      || nextCValue=="OR") {
-      // Logger.log("nextCValue = " + nextCValue);
-      break;
-    }
-    cellCol -= 1;
-  } while (cellCol >= columnLimit)
-  if (!nextCell.getValue()) {
-    endParse = true;
-    Logger.log("endParse = " + endParse);
-  }
-  return [nextCell, endParse, cellCol];
-}
-function drawIfOr(c) {
-  c.setHorizontalAlignment("right");
-  if (c.offset(0,1).isBlank()) {
-    c.offset(0,1).insertCheckboxes()
-      .setBorder(false,true,false,false,false,false,
-      "grey",SpreadsheetApp.BorderStyle.SOLID_THICK);
-    c.setBorder(null,null,null,true,false,false,
-      "grey",SpreadsheetApp.BorderStyle.SOLID_THICK);
-  }
-  if (c.offset(0,2).isBlank()) {
-    c.offset(0,2).setValue("some condition");
-  }
-}
-function drawAnd(c) {
-  c.setHorizontalAlignment("right");
-  if (c.offset(0,1).isBlank()) {
-    c.offset(0,1).insertCheckboxes()
-      .setBorder(null,true,false, false, false,false,
-      "grey",SpreadsheetApp.BorderStyle.SOLID_THICK);
-    c.offset(0,1,1,4)
-      .setBorder(true,true,false,false,false,false,
-      "grey",SpreadsheetApp.BorderStyle.SOLID_THICK);
-    c.setBorder(null,null,null,true,false,false,
-      "grey",SpreadsheetApp.BorderStyle.SOLID_THICK);
-  }
-  if (c.offset(0,2).isBlank()) {
-    c.offset(0,2).setValue("some condition");
-  }
 }
--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 These GAS(Google Apps Script)/JavaScript code were tested in Google Sheets.
 
 
+### Legal Test 11 Summary
+Implemented "Features and UI specifications" 3 and 4 for "MEANS", "IS" and "IT IS".
+
+### Legal Test 12 to 18 Summary
+Implementing "Features and UI specifications" 5:  "Given a range containing IF/AND/OR text with checkboxes, set up the logic formula at top left, based on the IF/AND/OR words. =AND/OR(xxx) where xxx is the checkboxes."
 
 #### Legal Test 13 Summary
 The start function is testEdit.  The script then loops through the functions parseWords, processIf and getNextCell repeatedly to identify a keyword in each subsequent cell below the start cell in the active spreadsheet.  The predicate next to each keyword is logged.
@@ -17,14 +22,23 @@ Legal Test 13 problem fixed.
 But new unresolved problem: 2 IF keywords throw up 1 predicate, 3 IF keywords throw up 3 predicates, 4 IF keywords throw up 3 predicates.
 
 After resolving this new problem, the next goal is to include AND and form the right parentheses.
-### Legal Test 17 Summary
+#### Legal Test 17 Summary
 Legal Test 16 problem fixed and goals achieved.
 
 New problem: Output of Example 13 in Constitutive Rules has 3 predicates.  Current implementation of Example 13 delivers 4 predicates because one of the predicate is the checking predicate.
 
-Tentative solution to be implemented: Create a summary.history object based on the JavaScript Map object to store positions of all predicates, so that there is control over checking predicate conclusion of IF parentheses statements.  Map object's Key will be a array object containing position values.  Map object's Values will be the predicates.  summary.topLeft, summary.furthestRow and summary.furthestCol properties can be recorded.
+Tentative solution to be implemented for Legal Test 18: Create a summary.history object based on the JavaScript Map object to store positions of all predicates, so that there is control over checking predicate conclusion of IF parentheses statements.  Map object's Key will be a array object containing position values.  Map object's Values will be the predicates.  summary.topLeft, summary.furthestRow and summary.furthestCol properties can be recorded.
 
-Reworked solution: Due to requirement for top left of every IF statement to contain a summary logic, the new idea is to use an Array object `h.history` instead of a Map object.  Each element of Array will be [col, keyword, predicate], so it's an array of arrays.  The index of the Array will be the row number.
+Reworked solution for Legal Test 18: Due to requirement for top left of every IF statement to contain a summary logic, the new idea is to use an Array object `h.history` instead of a Map object.  Each element of Array will be [col, keyword, predicate], so it's an array of arrays.  The index of the Array will be the row number.
 ### Legal Test 18 Summary
-Goals: To use `sheet.getRange` and `getA1Notation` on the elements of the `h.history` Array.
+Goals: Besides what's stated in "Reworked solution for Legal Test 18", the goal is to use `sheet.getRange` and `getA1Notation` on the elements of the `h.history` Array.
+
+Legal Test 18 goals achieved.  "Features and UI specifications" 5's basic effects implemented.  What's left is to smooth onEdit behaviours.
+### Legal Test 19 Summary
+Possible goals:
+- implement clearRow to clear formatting of row of cells before drawing a new keyword.
+- convert floating anonymous function to method of h object.
+- merge processAnd and processOr functions if separate functions not required.
+- identify and finetune other onEdit behaviours.
+
 
