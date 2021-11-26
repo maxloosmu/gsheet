a9e8a4e9c0cd1cc9d6ec8970242a47df56317f4b
maxloosmu <maxloo@smu.edu.sg>
2021-11-26 16:50:58 +0800

    amended README, added Legal Test 21

---
+++ Legal Test 21.js
@@ -0,0 +1,176 @@
+class History {
+  constructor(history = []) {
+    this.history = history;
+  }
+}
+function onEdit(e) {
+  // Respond to Edit events on spreadsheet.
+  let c = e.range;
+  let sheet = SpreadsheetApp.getActiveSheet();
+  let h = new History();
+  drawWords(c);
+  let startCell = findStart(c);
+  sheet.getRange(1, 1).setValue(startCell.getValue());
+  h = scanDownwards(startCell, h);
+  sheet.getRange(1, 2).setValue(c.getA1Notation());
+  testHistory(h, sheet);
+  processHistory(h, sheet);
+}
+function drawWords(c) {
+  // Identify keywords for formatting.
+  cValue = c.getValue();
+  if (cValue=="IF" || cValue=="WHEN") {
+    drawIfWhenTop(c);
+    drawIfWhenOr(c);
+  }
+  else if (cValue=="OR") {
+    drawIfWhenOr(c);
+  }
+  else if (cValue=="AND") {
+    drawAnd(c);
+  }
+}
+function findStart(c) {
+  // Find the topLeft start of all keywords.
+  let nextCol = 0;
+  let maxCount = 5;
+  while (nextCol < maxCount) {
+    let getTopRight = c.offset(-1, nextCol);
+    let gtr = getTopRight.getValue();
+    if (checkKeyword(gtr)) {
+      c = getTopRight;
+      return findStart(c);
+    }
+    nextCol++;
+  }
+  if (nextCol == maxCount) {
+    let getTopLeft = c.offset(-1, -1);
+    let gtl = getTopLeft.getValue();
+    if (checkKeyword(gtl)) {
+      c = getTopLeft;
+      return findStart(c);
+    }
+  }
+  return c;
+}
+function checkKeyword(cValue){
+  return (cValue=="IF" || cValue=="OR"
+    || cValue=="AND" || cValue=="WHEN");
+}
+function scanDownwards(c, h) {
+  // Scan downwards for keywords.
+  h = parseWords(c, h);
+  let cellCol = 1;
+  let columnLimit = cellCol - c.getColumnIndex();
+  do {
+    let nextCellBelow = c.offset(1,cellCol);
+    let ncb = nextCellBelow.getValue();
+    if (checkKeyword(ncb)) {
+      c = nextCellBelow;
+      return scanDownwards(c, h);
+    }
+    cellCol -= 1;
+  } while (cellCol >= columnLimit)
+  return h;
+}
+function parseWords(c, h) {
+  // Put keywords into h.history Array.
+  let rightOfIf = c.offset(0,1);
+  let predValue = rightOfIf.getValue();
+  h.history[c.getRowIndex()] =
+    [c.getColumnIndex(), c.getValue(), predValue];
+  return h;
+}
+function processHistory(h, sheet) {
+  // Process the h.history Array.
+  let restart = true;
+  let rowBegin = rowStop = numOfRows = 0;
+  let buildRange = rangeString = "";
+  let columnNow = farCol = 1;
+  for (const element of h.history) {
+    if (element != null) {
+      farCol = getFurthest(farCol, element[0]);
+    }
+  }
+  while (columnNow <= farCol) {
+    for (const element of h.history) {
+      if (element != null) {
+        let row = h.history.indexOf(element);
+        let [col, keyword, predicate] = element;
+        if (columnNow==col && restart &&
+          (keyword=="IF" || keyword=="WHEN")
+          && rowBegin<row) {
+          restart = false;
+          rowBegin = row;
+        }
+        if (columnNow==col && !restart) {
+          rowStop = getFurthest(rowStop, row);
+          numOfRows = rowStop - rowBegin + 1;
+          buildRange = sheet.getRange(rowBegin,
+            columnNow+1, numOfRows, 1);
+          rangeString = buildRange.getA1Notation();
+          sheet.getRange(rowBegin-1, columnNow)
+            .setValue("=" + keyword.toLowerCase()
+            + "(" + rangeString + ")");
+        }
+      }
+    }
+    columnNow++;
+    restart = true;
+    rowStop = 0;
+  }
+}
+function getFurthest(prevIndex, index) {
+  if (prevIndex < index) return index;
+  else return prevIndex;
+}
+function testHistory(h, sheet) {
+  // Test the h.history Array.
+  let lastRow = 1;
+  let farCol = 1;
+  for (const element of h.history) {
+    if (element != null) {
+      let row = h.history.indexOf(element);
+      let [col, keyword, predicate] = element;
+      lastRow = row;
+      farCol = getFurthest(farCol, col);
+    }
+  }
+  sheet.getRange(1, 3).setValue(h.history.toString());
+}
+function drawIfWhenTop(c) {
+  let topOfIf = c.offset(-1,0);
+  if (topOfIf.isBlank()) {
+    topOfIf.insertCheckboxes();
+  }
+}
+function drawIfWhenOr(c) {
+  c.offset(0,0,1,9).clearFormat();
+  c.setHorizontalAlignment("right");
+  if (c.offset(0,1).isBlank()) {
+    c.offset(0,1).insertCheckboxes()
+      .setBorder(false,true,false,false,false,false,
+      "grey",SpreadsheetApp.BorderStyle.SOLID_THICK);
+    c.setBorder(null,null,null,true,false,false,
+      "grey",SpreadsheetApp.BorderStyle.SOLID_THICK);
+  }
+  if (c.offset(0,2).isBlank()) {
+    c.offset(0,2).setValue("some condition");
+  }
+}
+function drawAnd(c) {
+  c.offset(0,1,1,4)
+    .setBorder(true,true,false,false,false,false,
+    "grey",SpreadsheetApp.BorderStyle.SOLID_THICK);
+  c.setHorizontalAlignment("right");
+  if (c.offset(0,1).isBlank()) {
+    c.offset(0,1).insertCheckboxes()
+      .setBorder(null,true,false, false, false,false,
+      "grey",SpreadsheetApp.BorderStyle.SOLID_THICK);
+    c.setBorder(null,null,null,true,false,false,
+      "grey",SpreadsheetApp.BorderStyle.SOLID_THICK);
+  }
+  if (c.offset(0,2).isBlank()) {
+    c.offset(0,2).setValue("some condition");
+  }
+}
--- README.md
+++ README.md
@@ -3,11 +3,22 @@ These GAS(Google Apps Script)/JavaScript code were tested in Google Sheets.  Pas
 
 
 ### Legal Test 11 Summary
-Implemented "Features and UI specifications" 3 and 4 for "MEANS", "IS" and "IT IS".
+Implemented "Features and UI specifications" 3 and 4 for "MEANS", "IS" and "IT IS":
 
-### Legal Test 12 to 20 Summary
-Implementing "Features and UI specifications" 5:  "Given a range containing IF/AND/OR text with checkboxes, set up the logic formula at top left, based on the IF/AND/OR words. =AND/OR(xxx) where xxx is the checkboxes."
+"A new "MEANS" or "IS" starts a constitutive rule, so create a T by colouring the borders."
 
+"Similarly a new "IT IS" starts a constitutive rule."
+
+### Legal Test 12 to 21 Summary
+Implementing "Features and UI specifications" 2, 5, 7, 10:
+
+"A new "IF" or "WHEN" should create a checkbox in the cell to the right."
+
+"Given a range containing IF/AND/OR text with checkboxes, set up the logic formula at top left, based on the IF/AND/OR words. =AND/OR(xxx) where xxx is the checkboxes."
+
+"Handle nested structures – example 13 of the constitutive rules."
+
+"Basically the invariant is that we always want the =OR/AND(...) checkboxes to be up to date and correct with whatever is below them."
 #### Legal Test 18 Summary
 Goals: Besides what's stated in "Reworked solution for Legal Test 18", the goal is to use `sheet.getRange` and `getA1Notation` on the elements of the `h.history` Array.
 
@@ -28,3 +39,21 @@ The array `h.history` cannot store historic values.  Evidence for this is record
 
 Goals for Legal Test 21:
 To find a way to parse the keywords without storing them.
+#### Legal Test 21 Summary
+Possibility of doing a backwards search of keywords using the `findStart` function is confirmed.  The first keyword must start from column 2 of the spreadsheet because if not, Google Sheets code execution will fail without warning - meaning, there is no response from the Sheet.
+
+Next Steps:
+Implement a forward processing of keywords from the start keyword.  Possibility of reusing the getNextCell function from Legal Test 19.
+
+Result:
+Backwards search using `findStart` and forward scan using `scanDownwards` implemented successfully.  `h.history` now works not because it records keywords between edits, but by recording all the keywords in the Legal Spreadsheet when they are all parsed from scratch for every edit.  Implemented processing for "IF", "OR", "AND", "WHEN" keywords.
+### Legal Test 22 to 23 Summary
+Implementing "Features and UI specifications" 3 and 4 again, with the additional 6 and 8:
+
+"Similarly, a constitutive rule needs a checkbox to span the children."
+
+"Handle the situation where the user adds a new row and then writes in a new OR/AND/UNLESS; currently this creates a checkbox to the right; need to update checkbox above."
+#### Legal Test 22 Summary
+Goals:
+Implement "MEANS", "IS", "IT IS", "UNLESS" with topLeft logic equation.
+
